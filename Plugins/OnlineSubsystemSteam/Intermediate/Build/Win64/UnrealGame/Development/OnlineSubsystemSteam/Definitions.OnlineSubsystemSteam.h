4e65c51cad5a1df40b895fcac768fb850a42d192
PofP7 <67156144+PofP7@users.noreply.github.com>
2023-08-26 23:20:59 +0700

    Edits

--- Plugins/OnlineSubsystemSteam/Intermediate/Build/Win64/UnrealGame/Development/OnlineSubsystemSteam/Definitions.OnlineSubsystemSteam.h
+++ Plugins/OnlineSubsystemSteam/Intermediate/Build/Win64/UnrealGame/Development/OnlineSubsystemSteam/Definitions.OnlineSubsystemSteam.h
@@ -45,7 +45,7 @@
 #define WITH_CEF3 1
 #define WITH_LIVE_CODING 1
 #define UE_LIVE_CODING_ENGINE_DIR "D:\\Program Files (x86)\\UE_5.1\\Engine"
-#define UE_LIVE_CODING_PROJECT "D:\\UE5_Projects\\FuryForDev\\Fury.uproject"
+#define UE_LIVE_CODING_PROJECT "D:\\UE5_Projects\\Fury\\FPS5\\Fury.uproject"
 #define WITH_CPP_MODULES 0
 #define WITH_CPP_COROUTINES 0
 #define WITH_PROCESS_PRIORITY_CONTROL 0
@@ -54,7 +54,7 @@
 #define UBT_COMPILED_PLATFORM Win64
 #define UBT_COMPILED_TARGET Game
 #define UE_APP_NAME "UnrealGame"
-#define UE_ENGINE_DIRECTORY "../../../../Program Files (x86)/UE_5.1/Engine/"
+#define UE_ENGINE_DIRECTORY "../../../../../Program Files (x86)/UE_5.1/Engine/"
 #define NDIS_MINIPORT_MAJOR_VERSION 0
 #define WIN32 1
 #define _WIN32_WINNT 0x0601
